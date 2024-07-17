7bfe955620242ad4dcfa2baf0a71a99268b5510f
BioCarmen <carmen@biorender.com>
2024-07-17 11:01:58 -0400

    build: change

--- scripts/nx-release.ts
+++ scripts/nx-release.ts
@@ -7,7 +7,7 @@ const COMMIT_HASH = process.argv[2];
 
   try {
     const versionOutput = await releaseVersion({
-      dryRun: false,
+      dryRun: true,
       verbose: true,
       gitCommit: false,
       gitTag: true,
@@ -19,9 +19,8 @@ const COMMIT_HASH = process.argv[2];
     await executeCommand(
       `git tag -f last-release ${COMMIT_HASH} && git push origin last-release --force && git push --tags`,
       'Pushed last-release tag and pushed tags for each published package',
-      'Failed to push last-release tag, or failed to push tags for each published package',
+      'Failed to push last-release tag, or failed to push tags for each published package'
     );
-
   } catch (err) {
     console.log('nx release failed with error', err);
   }
@@ -31,13 +30,15 @@ export async function executeCommand(
   command: string,
   successMessage?: string,
   failureMessage?: string,
-  showConsoleOutput = true,
+  showConsoleOutput = true
 ): Promise<string> {
   const { exec } = await import('child_process');
   return new Promise((resolve, reject) => {
     const childProcess = exec(command, (error, stdout, _stderr) => {
       if (error) {
-        const errorMessage = failureMessage ? `${failureMessage}: ${error.message}` : error.message;
+        const errorMessage = failureMessage
+          ? `${failureMessage}: ${error.message}`
+          : error.message;
         console.error(errorMessage);
         reject(errorMessage);
         return;
@@ -48,7 +49,7 @@ export async function executeCommand(
       resolve(stdout.trim());
     });
     if (showConsoleOutput) {
-      childProcess.stdout!.on('data', data => {
+      childProcess.stdout!.on('data', (data) => {
         console.log(data.toString());
       });
     }
